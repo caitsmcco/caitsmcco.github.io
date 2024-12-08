91b5ccaff8e89edb9bc5a8dab22a8571112682fb
caitsmcco <cmcconnery@outlook.com>
2024-12-08 14:19:33 -0700

    WAWAWAWA

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -3,21 +3,25 @@ const data = {
         "title":"Kaylee's Love Guesser"
         ,"author":"Group 15"
         ,"scheme":"#95b8d1"
+        ,"code":"https://github.com/kayleenasser/CPSC-581-Project-1"
     }
     ,"2": {
         "title":"Green Thumb Unlock"
         ,"author":"Group 16"
         ,"scheme":"#b8e0d4"
+        ,"code":"https://glitch.com/edit/#!/green-thumb-unlock"
     }
     ,"3": {
         "title":"Breakfast Now!"
         ,"author":"Caitlin McConnery"
         ,"scheme":"#d6eadf"
+        ,"code":"https://glitch.com/edit/#!/breakfast-now"
     }
     ,"4": {
         "title":"Jack's Snacks"
         ,"author":"Group 15"
         ,"scheme":"#eac4d5"
+        ,"code":"https://github.com/caitsmcco/jacks-snacks"
     }
     ,"5": {
         "title":"Sketchbook"
@@ -26,29 +30,37 @@ const data = {
     }
 };
 
+var thisItem = data[1];
+
 function loadContent(obj)   {
     var top = document.getElementById("selected");
     var box = document.getElementById("s_content");
     var navbar = document.getElementById("navbar");
     var title = document.getElementById('s_title');
     var author = document.getElementById('s_author');
-    
+    thisItem = data[obj];
     var selectedItem = data[obj];
     title.innerHTML = selectedItem["title"];
     author.innerHTML = selectedItem["author"];
     navbar.style.backgroundColor = selectedItem["scheme"];
     box.style.backgroundColor = selectedItem["scheme"];
     top.style.backgroundColor = selectedItem["scheme"];
-    loadInfo("a","b1");
+    loadInfo("b1");
 };
 
-async function loadInfo(obj,type){
+async function loadInfo(type){
+    if (type == "b5"){
+        window.open(thisItem["code"])
+        return;
+    }
     var buttons = document.getElementById("s_options").children;
     for (var i = 0; i < buttons.length; i++){
         buttons[i].style.backgroundColor = "#00000000";
     }
     document.getElementById(type).style.backgroundColor = "rgb(254, 255, 236, 0.4)";
-    var ref = await fetch("ab1.html");
-    var html = await ref.text();
-    document.getElementById('s_body').insertAdjacentHTML("beforeend", html);
+    element = document.getElementById("s_body");
+    filename = selected + "/" + type + ".html";
+    fetch(filename).then(res => {
+        element.innerHTML = res; 
+    });
 };
